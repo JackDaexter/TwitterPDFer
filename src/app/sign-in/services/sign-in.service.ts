8bbe3f8bb5a5046338d5a9c21abfb9d8027f25e2
Franck Albert Ngounou Kotchap <fngounou@etud.u-pem.fr>
2022-01-12 14:05:17 +0100

    - thread

--- src/app/sign-in/services/sign-in.service.ts
+++ src/app/sign-in/services/sign-in.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class SignInService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  rootURL = '/api';
+
+  getUsers() {
+    return this.http.get(this.rootURL + '/users');
+  }
+
+  getElement() {
+    return this.http.get(this.rootURL);
+  }
+
+  addUser(user: any) {
+    return this.http.post(this.rootURL + '/user', {user});
+  }
 }
