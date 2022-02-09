3a4685ef09e96d031c0a7c8914fb5fe18a5a2009
Franck Albert Ngounou Kotchap <fngounou@etud.u-pem.fr>
2022-02-09 10:28:48 +0100

    - Try debug ArrayBuffer transfert problem

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const load = require("./webscrapping/load.js");
 const app = express();
 const port = 3080;
+const fs = require('fs')
 const hostname = '127.0.0.1';
 
 // create application/json parser
@@ -13,14 +14,18 @@ app.get('/',(req,res) => {
 });
 
 app.post('/link',async (req,res) => {
- 
-    var tweetForm = await load.launch(req.body.link,req.body.type);
-    res.set('Content-Type', "application/pdf");
-    res.send({"pdf" : tweetForm});
+    var fs = require('fs')
+    var pdf;
+    pdf = fs.readFileSync(__dirname + '/thread2.pdf');
+        
+    //var tweetForm = await load.launch(req.body.link,req.body.type);
+    res.contentType('application/pdf');
+    res.send({"pdf" : pdf});
 })
 
 app.post('/user',(req,res) => {
     console.log(req.body);
+    res.write
     res.send({"enter" : "pass"});
 });
 
