445f6961a182e568c20b21fd94dfb5e49fdc186a
Franck Albert Ngounou Kotchap <fngounou@etud.u-pem.fr>
2022-04-03 01:06:46 +0200

    Add download raw file

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const load = require("./webscrapping/load.js");
 const app = express();
 const port = 3080;
 const hostname = '127.0.0.1';
-
+const fs = require('fs');
+const Tweet = require('./webscrapping/Tweet/tweet');
 // create application/json parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
@@ -14,8 +15,17 @@ app.get('/',(req,res) => {
 
 app.post('/link',async (req,res) => {
     var tweetForm = await load.launch(req.body.link,req.body.type);
-    res.contentType('application/pdf');
-    res.send(tweetForm);
+
+    if(req.body.type == 1){
+        res.contentType('application/pdf');
+        res.send(tweetForm);
+    }
+    else{
+        saveDateToFile(tweetForm)
+        res.contentType('text/plain');
+        res.json(tweetForm.join("------------------------------------------- \n").)
+    }
+    
 })
 
 app.post('/user',(req,res) => {
@@ -23,6 +33,16 @@ app.post('/user',(req,res) => {
     res.write
     res.send({"enter" : "pass"});
 });
+
+function saveDateToFile(data)
+{
+    fs.writeFile("./test.txt",data.join("------------------------------------------- \n"), err => {
+        if(err){
+            console.log(err);
+            return
+        }
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
